eeb89bd7b5da1383f99ae44597f1901dbe3ded53
Juanpi-Fernandez <jpprogrammer1@gmail.com>
2024-04-26 19:51:43 -0300

    descargar pdf si es celular

--- src/app/presupuesto/page.tsx
+++ src/app/presupuesto/page.tsx
@@ -1,16 +1,31 @@
 'use client';
 import React from 'react'
 import { Template } from './components/Template';
-import { PDFViewer } from '@react-pdf/renderer';
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
+import isMobile from 'ismobilejs';
 
-const Page = (props:any) => {
-  const {searchParams} = props || {};
-  const {data} = searchParams || {};
+const Page = (props: any) => {
+  const { searchParams } = props || {};
+  const { data } = searchParams || {};
   const parseData = JSON.parse(data);
-  
+
   return (
-    <PDFViewer style={{ height: '100vh', width: '100vh', margin: '0 auto' }}>
-      <Template data={parseData} />
-    </PDFViewer>)
+    <>
+      {isMobile().phone
+        ? <PDFDownloadLink document={<Template data={parseData} />} fileName={`${parseData.fecha}-${parseData.cliente}-presupuesto.pdf`}>
+          {({ blob, url, loading, error }) =>
+            <div>
+              {loading ? 'Loading document...' : 'Download now!'}<br />
+              <a className='btn btn-success' download={url}>Descargar</a>
+            </div>
+
+          }
+        </PDFDownloadLink>
+        : <PDFViewer style={{ height: '100vh', width: '100vh', margin: '0 auto' }}>
+          <Template data={parseData} />
+        </PDFViewer>
+      }
+    </>
+  )
 }
 export default Page;
