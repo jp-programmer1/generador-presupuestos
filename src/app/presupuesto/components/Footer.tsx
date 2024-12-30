04021471c8dad436e9522121d040d08fbb7277f0
Juan Pablo Fernandez <jpprogrammer1@gmail.com>
2024-12-30 14:53:01 -0300

    add warning in pdf

--- src/app/presupuesto/components/Footer.tsx
+++ src/app/presupuesto/components/Footer.tsx
@@ -6,7 +6,11 @@ export const Footer = ({total}:any) => {
     <View style={{ padding: 20 }}>
       <View style={{ border: '1px solid #E1E1E1', padding: 10 }}>
         <View style={{ flexDirection: 'row' }}>
-          <Text style={{ fontSize: 12, textDecoration: 'underline' }}>LOS PRECIOS PUEDEN SUFRIR CAMBIOS SIN PREVIO AVISO </Text>
+          <View>
+            <Text style={{ fontSize: 12, textDecoration: 'underline' }}>LOS PRECIOS PUEDEN SUFRIR CAMBIOS SIN PREVIO AVISO </Text>
+            <Text style={{ fontSize: 10, color: '#C21807', textDecoration: 'underline' }}>Materiales eléctricos, electrónicos y de refrigeración no tienen garantía.</Text>
+          </View>
+          
           <Text style={{ fontSize: 15, marginLeft: 10 }}>TOTAL: $ {total}</Text>
         </View>
         <Text style={{fontSize: 10, marginTop: 10}}>De Roberto Leveque. Cuit: 20270932186 Boulevard Belgrano 371 Costa de Araujo</Text>
